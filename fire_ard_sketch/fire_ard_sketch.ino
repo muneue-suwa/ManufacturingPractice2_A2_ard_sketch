add87da8faea5193c747aa4fcbe3407a51af4297
Dongsik <dongsiku+git@gmail.com>
2018-12-21 22:19:57 +0900

    Add the button on-off feature

--- fire_ard_sketch/fire_ard_sketch.ino
+++ fire_ard_sketch/fire_ard_sketch.ino
@@ -1,45 +1,58 @@
 const int LED_PIN = 3;
 const int RPI_INPUT_PIN = 12;
+const int BUTTON_INPUT_PIN = 5;
 int isOn = 0;
 int led_values[] = {255, 100, 255, 50};
 int led_value_fig = 0;  // 0 - 3
 int rpi_high_times = 0;
+int button_high_times = 0;
 
 void setup() {
   pinMode(RPI_INPUT_PIN, INPUT);
+  pinMode(BUTTON_INPUT_PIN, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  if(rpi_high_times < 0){
-    // do not change status immediately after status changed
-    rpi_high_times += 1;
-  }else if(digitalRead(RPI_INPUT_PIN) == HIGH){
-    rpi_high_times += 1;
-    // Serial.println("RPI_HIGH");
-    if(rpi_high_times > 3){  // pushing 0.4 secs
-      isOn = 1 - isOn;
-      rpi_high_times = -50; // wait 5 secs after status changed
-    }
-  }else{
-    rpi_high_times = 0;
-  }
+  change_isOn(&rpi_high_times, RPI_INPUT_PIN);
+  change_isOn(&button_high_times, BUTTON_INPUT_PIN);
 
   if(isOn > 0){
     fire(&led_value_fig);
   }else{
     analogWrite(LED_PIN, 0); // LED OFF
   }
-  Serial.print("rpi_high_times: ");
+  Serial.print("rpi_high_times   : ");
   Serial.println(rpi_high_times);
-  delay(100);
+  Serial.print("button_high_times: ");
+  Serial.println(button_high_times);
+  Serial.print("isOn: ");
+  Serial.println(isOn);
+  delay(500);
 }
 
-int fire(int *value_fig){
+void fire(int *value_fig){
     analogWrite(LED_PIN, led_values[*value_fig]); // 0 - 255
     *value_fig += 1;
     if(*value_fig > 3){
       *value_fig = 0;
     }
+    return;
 }
 
+void change_isOn(int *high_times, int INPUT_PIN){
+  if(*high_times < 0){
+    // do not change status immediately after status changed
+    *high_times += 1;
+  }else if(digitalRead(INPUT_PIN) == HIGH){
+    *high_times += 1;
+    // Serial.println("RPI_HIGH");
+    if(*high_times > 3){  // pushing 0.4 secs
+      isOn = 1 - isOn;
+      *high_times = -50; // wait 5 secs after status changed
+    }
+  }else{
+    *high_times = 0;
+  }
+  return;
+}
